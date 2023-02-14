52e5a3b3ca007715b4fd1cec85e3abf607d0f73d
MattRas10 <mattraseta@yahoo.com>
2023-02-13 21:31:45 -0600

    More playback code

--- app/src/main/java/com/dillxn/tactilesynth/PlaybackHandler.java
+++ app/src/main/java/com/dillxn/tactilesynth/PlaybackHandler.java
@@ -1,10 +1,19 @@
 package com.dillxn.tactilesynth;
 
+import android.Manifest;
+import android.content.pm.PackageManager;
 import android.media.MediaRecorder;
 import android.media.AudioTrack;
 import android.media.AudioFormat;
 import android.media.AudioManager;
+import android.media.AudioRecord;
+import android.content.Context;
+import android.app.Activity;
+
+import androidx.core.app.ActivityCompat;
+
 import java.util.ArrayList;
+
 import org.json.JSONArray;
 
 /*
@@ -16,7 +25,9 @@ import org.json.JSONArray;
     to encode into mp3.
  */
 public class PlaybackHandler {
-    private MediaRecorder mediaRecorder;
+    Context context;
+    private AudioRecord audioRecorder;
+    byte[] newRecording;
     //to handle multiple recordings at once we will need to have an array of AudioTracks.
     private ArrayList<AudioTrack> tracks = new ArrayList<>();
 
@@ -24,20 +35,41 @@ public class PlaybackHandler {
     private static final int CHANNEL_CONFIG = AudioFormat.CHANNEL_OUT_MONO;
     private static final int AUDIO_FORMAT = AudioFormat.ENCODING_PCM_8BIT;
     private static final int STREAM_TYPE = AudioManager.STREAM_MUSIC;
+    private static final int BUFFER_SIZE = AudioRecord.getMinBufferSize(SAMPLING_RATE, CHANNEL_CONFIG, AUDIO_FORMAT);
     private static final int MODE = AudioTrack.MODE_STREAM;
 
     Database db;
     JSONArray recordings;
 
-    byte[] newRecording;
+    public PlaybackHandler(Context c, Activity a, Database db) {
+        this.db = db;
+        this.context = c;
 
+        if (ActivityCompat.checkSelfPermission(context, Manifest.permission.RECORD_AUDIO) != PackageManager.PERMISSION_GRANTED) {
+            ActivityCompat.requestPermissions(a, new String[] {"Manifest.permission.RECORD_AUDIO"}, 0);
+            /*
+            possibly need to handle the permission checking in the main activity that this will occur in. UNless I restructure this
+            to be it's own activity. Not sure what that entails or even means so I need to check that out next. The permission request
+            will return a result to the onPermissionRequestResults with the code provided. This method needs to be overriden and handle
+            what happens if accepted/rejected.
+             */
 
-    public PlaybackHandler(){
-        mediaRecorder = new MediaRecorder();
+        }
+        audioRecorder = new AudioRecord(MediaRecorder.AudioSource.DEFAULT, SAMPLING_RATE, CHANNEL_CONFIG, AUDIO_FORMAT, BUFFER_SIZE);
+
         //get the json object where we will store file names, then if that is not empty. Build an audioTrack.
 
     }
     //create a method to record and get the byte[] and store it in newRecording. Then figure out how to save that.
+    public void startRecording(){
+        audioRecorder.startRecording();
+        newRecording = new byte[BUFFER_SIZE];
+        audioRecorder.read(newRecording, 0, BUFFER_SIZE);
+    }
+    public void stopRecording(){
+        audioRecorder.stop();
+        audioRecorder.release();
+    }
 
 
 
